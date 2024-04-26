665cba15432b111d7586917c4eb7e86f074b47d7
Sarvesh Kumar Roy <sarveshkumarroy1@gmail.com>
2024-04-26 19:17:26 +0530

    Implemented leadkedCred Detail Api

--- api/leaked_credentials/cred.controller.js
+++ api/leaked_credentials/cred.controller.js
@@ -1,5 +1,5 @@
-const { Sequelize} = require('sequelize');
-const { db , initializeDb } = require('../../_helpers/db');
+const { Sequelize } = require("sequelize");
+const { db, initializeDb } = require("../../_helpers/db");
 initializeDb();
 
 //Write all the controller functions and export them
@@ -40,36 +40,36 @@ initializeDb();
         "no_leaks": 5
     }'
  */
-async function create (req, res) {
-    // Validate request
-    if(!req.body){
-        res.status(400).json({ message: "Content can not be empty!" });
-        return;
-    }
-    //Create a new leakCred
-    try {
-        //Create a leakCred
-        const leakCred = {
-            id: req.body.id,
-            email: req.body.email,
-            created_at: req.body.created_at,
-            updated_at: req.body.updated_at,
-            created_by: req.body.created_by,
-            updated_by: req.body.updated_by,
-            no_leaks: req.body.no_leaks
-        };
-        db.models.LeakCred.create(leakCred)
-        .then((data)=>{
-            res.status(201).json(data);
-        })
-        .catch((error)=>{
-            res.status(500).json({ message: error.message });
-        });
-    } catch (error) {
+async function create(req, res) {
+  // Validate request
+  if (!req.body) {
+    res.status(400).json({ message: "Content can not be empty!" });
+    return;
+  }
+  //Create a new leakCred
+  try {
+    //Create a leakCred
+    const leakCred = {
+      id: req.body.id,
+      email: req.body.email,
+      created_at: req.body.created_at,
+      updated_at: req.body.updated_at,
+      created_by: req.body.created_by,
+      updated_by: req.body.updated_by,
+      no_leaks: req.body.no_leaks,
+    };
+    db.models.LeakCred.create(leakCred)
+      .then((data) => {
+        res.status(201).json(data);
+      })
+      .catch((error) => {
         res.status(500).json({ message: error.message });
-        return;
-    }
-};
+      });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+    return;
+  }
+}
 
 // Description:
 // This Function retrieves all LeakCred records from the database.
@@ -92,63 +92,70 @@ async function create (req, res) {
 //     ...
 // ]
 
-
 //This returns total number of leaks per email address.
-const {Op} = Sequelize
-async function leakCounts(req, res){
-    try {
-      const count  = await db.models.LeakCred.count({
-        where: {
-          email: {
-            [Op.eq]: req.query.email
-          }
+const { Op } = Sequelize;
+async function leakCounts(req, res) {
+  const { pageSize = 10 } = req.query;
+  try {
+    const count = await db.models.LeakCred.count({
+      where: {
+        email: {
+          [Op.eq]: req.query.email,
         },
-      });
-      res.status(200).json({ count });
-    } catch (error) {
-      console.error('Error counting leaked emails:', error);
-      res.status(500).json({ error: 'Internal server error' });
-    }
+      },
+    });
+    const totalCount = await db.models.LeakCred.count();
+    const totalPages = Math.ceil(totalCount / Number(pageSize));
+
+    res.status(200).json({ count, totalPages });
+  } catch (error) {
+    console.error("Error counting leaked emails:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 }
 
 //This returns all existing records of user in pagintion method.
-async function findAll (req, res)  {
-    const { page = 1, pageSize = 10 } = req.query;
-    const offset = Number(Number(page) - 1) * Number(pageSize);
-    try {
-        await db.models.LeakCred.findAll({
-                offset,
-                limit: Number(pageSize)
-        })
-        .then((data)=>{
-            res.status(200).json(data);
-        }).catch((error)=>{
-            res.status(500).json({ message: error.message });
-        });
-    } catch (error) {
+async function findAll(req, res) {
+  const { page = 1, pageSize = 10 } = req.query;
+  const offset = Number(Number(page) - 1) * Number(pageSize);
+  try {
+    const totalCount = await db.models.LeakCred.count();
+    const totalPages = Math.ceil(totalCount / Number(pageSize));
+    await db.models.LeakCred.findAll({
+      offset,
+      limit: Number(pageSize),
+    })
+      .then((data) => {
+        res.status(200).json({ data, totalPages });
+      })
+      .catch((error) => {
         res.status(500).json({ message: error.message });
-    }
+      });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 }
 
 //This returns all existing emails of user in pagintion method.
-async function totalEmails(req, res){
-    const { page = 1, pageSize = 10 } = req.query;
-    const offset = Number(Number(page) - 1) * Number(pageSize);
+async function totalEmails(req, res) {
+  const { page = 1, pageSize = 10 } = req.query;
+  const offset = Number(Number(page) - 1) * Number(pageSize);
 
-    try {
-        const emails = await db.models.LeakCred.findAll({
-          attributes: ['email'],
-          group: ['email'],
-          offset,
-          limit: Number(pageSize)
-        });
-    
-        res.json({ emails });
-      } catch (error) {
-        console.error('Error fetching tracked emails:', error);
-        res.status(500).json({ error: 'Internal server error' });
-      }
+  try {
+    const totalCount = await db.models.LeakCred.count();
+    const totalPages = Math.ceil(totalCount / Number(pageSize));
+    const emails = await db.models.LeakCred.findAll({
+      attributes: ["email"],
+      group: ["email"],
+      offset,
+      limit: Number(pageSize),
+    });
+
+    res.json({ emails, totalPages });
+  } catch (error) {
+    console.error("Error fetching tracked emails:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 }
-
 
 module.exports = { create, findAll, leakCounts, totalEmails }; // Export the controller functions
