d269189e18543853e1807133e8c431ce489c72f3
B <b@fed.fedora>
2024-04-28 23:37:54 +0530

    fix

--- api/user_request/userRequest.service.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const userRequestRouter = express.Router();
-const userRequestController  = require('./userRequest.controller');
-
-userRequestRouter.post('/addUserRequest', userRequestController.create);
-userRequestRouter.get('/getAllUserRequests',userRequestController.getAllUserRequests);
-
-
-module.exports = userRequestRouter;
