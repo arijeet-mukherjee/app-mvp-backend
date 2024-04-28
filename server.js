59be9cb84f06bdc207e2970b9f35b23e2109cc84
Arijeet Mukherjee <ari123acess@gmail.com>
2024-04-28 18:09:42 +0530

    Update server.js to use websocket server and app server

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const http = require('http');
 const app = express();
+const server = http.createServer(app);
+const WebSocketManager = require('./_middleware/websocketManager');
+const wssManager = new WebSocketManager(server);
 
 const { appRouter } = require('./api/index');
 
 require('dotenv').config();
 
+app.use((req, res, next) => {
+    req.wssManager = wssManager;
+    next();
+});
+
 app.use('/api', appRouter);
 
-app.listen(process.env.PORT, () => {
+server.listen(process.env.PORT, () => {
     console.log(`App listening on port ${process.env.PORT}!`);
 });
