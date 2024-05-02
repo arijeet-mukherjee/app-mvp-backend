f760f2b52b63849b0bb1fcee56cf7595acd28d80
Arijeet Mukherjee <ari123acess@gmail.com>
2024-05-02 19:57:01 +0530

    final changes to pages crud done

--- api/pages/pages.controller.js
+++ api/pages/pages.controller.js
@@ -2,10 +2,6 @@ const { db, initializeDb } = require('../../_helpers/db');
 const { isEntryPresentForLanguage } = require('../../_util/index');
 initializeDb();
 
-//VALIDATE
-//REMOVE CONSOLE LOG
-//AUTOMATE ID
-//UPDATE RESPONSE MANIPULATION
 async function getAllPages(req, res) {
     try {
         await db.models.Pages.findAll()
@@ -33,15 +29,17 @@ async function getAllPageDetails(req, res) {
 }
 
 async function getPageDetailsByPageId(req, res) {
-    if (!req.body) {
+    if (!req.body.page_id || !req.body.language_id) {
         res.status(400).json({ message: "Content can not be empty!" });
         return;
     }
     try {
+        const page_id = req.body.page_id.typeOf === 'number' ? req.body.page_id : parseInt(req.body.page_id);
+        const language_id = req.body.language_id.typeOf === 'number' ? req.body.language_id : parseInt(req.body.language_id);
         await db.models.PageDetail.findAll({
             where: {
-                page_id: req.body.page_id,
-                language_id: req.body.language_id
+                page_id: page_id,
+                language_id: language_id
             }
         })
             .then(data => {
@@ -61,20 +59,25 @@ async function createPage(req, res) {
     }
 
     try {
+        const id = await db.models.Pages.max('id').then(data => data ? data + 1 : 1).catch((error) => res.status(500).send(error));
+        const slug = req.body.slug.typeOf === 'string' ? req.body.slug : req.body.slug.toString();
+        const created_by = req.body.created_by.typeOf === 'number' ? req.body.created_by : parseInt(req.body.created_by);
+        const date = new Date();
+
         const newPage = {
-            id: req.body.id,
-            slug: req.body.slug,
-            created_by: req.body.created_by,
+            id: id,
+            slug: slug,
+            created_by: created_by,
             updated_by: null,
             deleted_at: null,
-            created_at: new Date(),
-            updated_at: null
+            created_at: date,
+            updated_at: date
         };
 
         await db.models.Pages.create(newPage)
             .then(data => {
-                console.log('Page details:', data);
                 res.status(200).json(data)
+                return;
             })
             .catch((error) => res.status(500).send(error));
     } catch (error) {
@@ -95,20 +98,29 @@ async function createPageDetails(req, res) {
             return;
         }
 
+        const id = await db.models.PageDetail.max('id').then(data => data ? data + 1 : 1).catch((error) => res.status(500).send(error));
+        const page_type = req.body.page_type.typeOf === 'string' ? req.body.page_type : req.body.page_type.toString();
+        const title = req.body.title.typeOf === 'string' ? req.body.title : req.body.title.toString();
+        const description = req.body.description.typeOf === 'string' ? req.body.description : req.body.description.toString();
+        const page_id = req.body.page_id.typeOf === 'number' ? req.body.page_id : parseInt(req.body.page_id);
+        const language_id = req.body.language_id.typeOf === 'number' ? req.body.language_id : parseInt(req.body.language_id);
+        const date = new Date();
+
         const newPageDetail = {
-            id: req.body.id + 24,
-            page_type: req.body.page_type,
-            title: req.body.title,
-            description: req.body.description,
-            language_id: req.body.language_id,
-            page_id: req.body.page_id,
-            created_at: new Date(),
-            updated_at: null
+            id: id,
+            page_type: page_type,
+            title: title,
+            description: description,
+            language_id: language_id,
+            page_id: page_id,
+            created_at: date,
+            updated_at: date
         };
+
         await db.models.PageDetail.create(newPageDetail)
             .then(data => {
-                console.log('Page details:', data);
-                res.status(200).json(data)
+                res.status(200).json(data);
+                return;
             })
             .catch((error) => res.status(500).send(error));
     } catch (error) {
@@ -123,38 +135,56 @@ async function updatePageDetails(req, res) {
         return;
     }
     try {
-        const pageDetailId = req.body.id;
-        await db.models.PageDetail.findAll({
+        const pageDetailId = req.body.id.typeOf === 'number' ? req.body.id : parseInt(req.body.id);
+        const language_id = req.body.language_id.typeOf === 'number' ? req.body.language_id : parseInt(req.body.language_id);
+
+        await db.models.PageDetail.findOne({
             where: {
-                id: req.body.id,
-                language_id: req.body.language_id
+                id: pageDetailId,
+                language_id: language_id
             }
         }).then(async (data) => {
-            console.log('Updated Page details:', data);
-            if (data.length === 0) {
+            if (!data) {
                 res.status(404).json({ message: "Page detail not found" });
                 return;
             }
+
+            const page_type = req.body.page_type.typeOf === 'string' ? req.body.page_type : req.body.page_type.toString();
+            const title = req.body.title.typeOf === 'string' ? req.body.title : req.body.title.toString();
+            const description = req.body.description.typeOf === 'string' ? req.body.description : req.body.description.toString();
+
             const updatedPageDetail = {
-                page_type: req.body.page_type,
-                title: req.body.title,
-                description: req.body.description,
-                language_id: req.body.language_id,
+                page_type: page_type,
+                title: title,
+                description: description,
+                language_id: language_id,
                 updated_at: new Date()
             };
 
             await db.models.PageDetail.update(updatedPageDetail, {
                 where: {
                     id: pageDetailId,
-                    language_id: req.body.language_id
+                    language_id: language_id
                 }
             })
-                .then(data => {
-                    console.log('Updated Page details:', data);
-                    res.status(200).json(data);
+                .then(async (data) => {
+                    if (data[0] === 1) {
+                        await db.models.PageDetail.findOne(
+                            {
+                                where: {
+                                    id: pageDetailId
+                                }
+                            }
+                        ).then((data) => {
+                            res.status(200).json(data);
+                            return;
+                        })
+                    }
                     return;
                 })
+                .catch((error) => res.status(500).send(error));
         })
+            .catch((error) => res.status(500).send(error));
 
     } catch (error) {
         res.status(500).send({ error: error.message });
@@ -168,19 +198,22 @@ async function deletePage(req, res) {
         return;
     }
     try {
+
+        const page_id = req.body.page_id.typeOf === 'number' ? req.body.page_id : parseInt(req.body.page_id);
+
         await db.models.PageDetail.destroy({
             where: {
-                page_id: req.body.id
+                page_id: page_id
             }
         })
             .then(async (data) => {
                 await db.models.Pages.destroy({
                     where: {
-                        id: req.body.id
+                        id: page_id
                     }
                 }).then(data => {
                     res.status(200).json({ message: "Page and associated page details deleted successfully." });
-                    return
+                    return;
                 })
             })
     } catch (error) {
