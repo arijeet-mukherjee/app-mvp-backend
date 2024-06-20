9cfdffe47bf31e184679b63cfff854fd0dd63c9f
Ayush Kedia <ayushkedia1990@gmail.com>
2024-06-20 20:52:01 +0530

    minor changes

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const config = {
     "database": {
         "host": `${process.env.DB_HOST || "localhost"}`,
         "port": `${process.env.DB_PORT || 3306}`,
-        "user": `${process.env.DB_USER || "root"}`,
+        "user": `${process.env.DB_USER || "freedb_rootsecdesk"}`,
         "password": `${"e2hZ5mZdF6be6$*"}`,
         "database": `${process.env.DB_NAME || "mydb"}`
     }
